5b43dba48d246cefa05b9cdd213d451f188ca44e
Douglas Porto <douglasalexandre7@gmail.com>
2021-02-26 17:32:44 -0300

    :wrench: Config: Configure styled-components

---
+++ src/pages/_app.tsx
@@ -0,0 +1,23 @@
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import GlobalStyles from 'styles/global'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>First Next Blog</title>
+        <link rel="shortcut icon" href="vercel.svg" />
+        <link rel="apple-touch-icon" href="vercel.svg" />
+        <meta
+          name="description"
+          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default App
